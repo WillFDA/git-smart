30f29145c8c813514e0a563b3b8a25d5ab5a30d1
William <william.deazevedopro@gmail.com>
2025-09-09 10:58:33 +0200

    misc(precommit): precommit skip test ci 3

--- src/home.tsx
+++ src/home.tsx
@@ -2,5 +2,7 @@ import { useState } from "react";
 
 export default function App() {
   const [state, setState] = useState(1);
+  if (!state) throw new Error("Super error");
+
   return <div>Hello world</div>;
 }
