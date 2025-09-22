5e79fd6f927280d49ad0a0d6fc684203c2470ae1
William <william.deazevedopro@gmail.com>
2025-09-22 09:40:32 +0200

    vite pwa useless

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "Git-smart",
-        description: "Une extension pour voir tes PR à review ou ouvertes",
-        icons: [
-          {
-            src: "/git-192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/git-512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: "build",
     rollupOptions: {
