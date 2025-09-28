d6b6efd870724e3a028d1240e9b375bb1f83ef27
William <111514062+WillFDA@users.noreply.github.com>
2025-09-28 21:59:12 +0200

    feat(api): github api and user / issues
    * feat(api): start of usage of api and first tests
    
    * lint(ultracite): ultracite
    
    * feat(api): hook get pull request from user login name
    
    * feat(dx): commit hook reset

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter, Route, Routes } from "react-router";
 import "./index.css";
+import { QueryClientProvider } from "@tanstack/react-query";
 import Layout from "./layout";
 import Access from "./routes/access";
 import { Dashboard } from "./routes/dashboard";
+import { queryClient } from "./services/api";
 
 const rootElement = document.getElementById("root");
 
@@ -15,12 +17,14 @@ if (!rootElement) {
 createRoot(rootElement).render(
   <StrictMode>
     <HashRouter>
-      <Routes>
-        <Route element={<Layout />} path="/">
-          <Route element={<Access />} path="/access" />
-          <Route element={<Dashboard />} path="/" />
-        </Route>
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route element={<Layout />} path="/">
+            <Route element={<Access />} path="/access" />
+            <Route element={<Dashboard />} path="/" />
+          </Route>
+        </Routes>
+      </QueryClientProvider>
     </HashRouter>
   </StrictMode>
 );
---
+++ src/services/github.ts
@@ -0,0 +1,40 @@
+import { useQuery } from "@tanstack/react-query";
+import { octokit } from "./api";
+
+export const getUser = async () => {
+  const response = await octokit.request("GET /user", {
+    headers: {
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  });
+  return response.data;
+};
+
+export const useGetUser = () => {
+  return useQuery({
+    queryKey: ["user"],
+    queryFn: getUser,
+  });
+};
+
+const getUserIssues = async (username: string) => {
+  const response = await octokit.request("GET /search/issues", {
+    q: `type:pr author:${username} state:open`,
+    per_page: 100,
+  });
+  return response.data;
+};
+
+export const getUserPullRequests = async (name: string) => {
+  const response = await getUserIssues(name);
+  return response;
+};
+
+export const useGetUserPullRequests = () => {
+  const { data: user } = useGetUser();
+  return useQuery({
+    queryKey: ["userPullRequests", user?.login],
+    queryFn: () => getUserPullRequests(user?.login || ""),
+    enabled: !!user?.login,
+  });
+};
